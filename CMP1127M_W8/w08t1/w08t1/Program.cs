98871219f80c5d77613a64d0c4e4601a3eb2411a
Hata-K <45198913+Hata-K@users.noreply.github.com>
2018-11-20 10:46:14 +0000

    W8
    Here are the doc from the programming and data structures workshop week 8.

---
+++ CMP1127M_W8/w08t1/w08t1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w08t1
+{
+	class Calculator
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("~~~~~~ Acme Calculator-o-Matic ~~~~~~");
+			Console.WriteLine("Enter two integers to add...");
+			int number1 = Convert.ToInt32(Console.ReadLine());
+			int number2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Answer: {0} \n", Calculator.add(number1, number2));
+
+			Console.WriteLine("Enter two doubles to add...");
+			double number3 = Convert.ToDouble(Console.ReadLine());
+			double number4 = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Answer: {0}", Calculator.add(number3, number4));
+			Console.ReadLine();
+		}
+
+		private static int add(int a, int b)
+		{
+			return (a + b);
+		}
+		private static double add(double a, double b)
+		{
+			return (a + b);
+		}
+	}
+}
+	

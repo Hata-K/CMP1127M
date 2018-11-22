b7644a79f9caf5c6a7d3144accf05de1d70eae19
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-11-22 16:13:11 +0000

    Week 09 - Task 02
    Commit which has the solution for week 09, task 02 of the programming and data structure module.

--- CMP1127M_W9/w09t2/w09t2/Program.cs
+++ CMP1127M_W9/w09t2/w09t2/Program.cs
@@ -10,6 +10,28 @@ namespace w09t2
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the size of the array you want to create: ");
+            int arrLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] intArr = new int[arrLength];
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write("Input {0} element in the array: ", (i + 1));
+                intArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+
+            DisplayElementsI(intArr);
+        }
+
+        private static void DisplayElementsI(int[] intArr)
+        {
+            Console.WriteLine("Displaying elements...");
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.WriteLine("Element - {0} : {1}", i, intArr[i]);
+            }
         }
     }
 }

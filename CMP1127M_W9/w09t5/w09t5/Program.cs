93bb37f889c23965e6aec16938883f4112a7bf3b
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-11-22 17:20:10 +0000

    Week 09 Tasks
    Commit which has the solution for week 09, task 05 of the programming and data structure module.

---
+++ CMP1127M_W9/w09t5/w09t5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w09t5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] intArr = new int[,] { { 2, 3}, { 2, 5}, { 7, 1}, { 4, 5 }};
+
+            DisplayElements(intArr);
+        }
+
+        private static void DisplayElements(int[,] Arr)
+        {
+            Console.WriteLine("2D int array...");
+            for (int i = 0; i < Arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < Arr.GetLength(1); j++)
+                {
+                    Console.Write(Arr[i,j]+ " ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}

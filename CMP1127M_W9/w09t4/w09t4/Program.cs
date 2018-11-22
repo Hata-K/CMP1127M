88c744d12f28a203ee62bf746cf75e67fce18f85
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-11-22 16:59:55 +0000

    Week 09 - Task 04
    Commit which has the solution for week 09, task 04 of the programming and data structure module.

---
+++ CMP1127M_W9/w09t4/w09t4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w09t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] stuScore = new int[30];
+
+            for (int i = 0; i < stuScore.Length; i++)
+            {
+                int rndScore = rnd.Next(1, 101);
+                stuScore[i] = rndScore;
+            }
+
+            outputAvg(stuScore);
+        }
+
+        private static void outputAvg(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            int avg = sum / (arr.Length);
+            Console.WriteLine("Average of the scores are: {0}", avg);
+        }
+    }
+}

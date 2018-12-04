1f9124cab9da8c0269c9b87df6a10f168383deaf
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-12-04 09:51:11 +0000

    w10b_t1
    Upload for task 01 of week 10 part B.

---
+++ CMP1127M_W10/w10b/w10b_t1/w10b_t1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w10b_t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialising the stack
+            Stack<char> charStack = new Stack<char>();
+
+            //Getting input from the user
+            Console.Write("Enter sequence of characters: ");
+            string userInput = Console.ReadLine();
+
+            //String is turned into a char array
+            char[] UIArray = userInput.ToArray();
+
+            //Each element is pushed into the stack
+            foreach (var x in UIArray)
+            {
+                charStack.Push(x);
+            }
+            Console.WriteLine("\n");
+
+            //Poping from the stack
+            //Geetting the inital length so poping doesn't effect the stack
+            int len = charStack.Count();
+            Console.WriteLine("Reveserd output using STACK is: ");
+            for (int i = 0; i < len; i++)
+            {                
+                Console.Write(charStack.Pop() + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
